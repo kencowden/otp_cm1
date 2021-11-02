91855f3b68476b1e7276b19672306ced75f98bd5
Ary Rosvall <aryrosvall@AryRosvall-2.local>
2021-11-02 07:05:34 -0700

    Add GenServer module

---
+++ count_on_me/lib/count_on_me/server.ex
@@ -0,0 +1,24 @@
+defmodule CountOnMe.Server do
+  use GenServer
+  alias CountOnMe.MapCore
+  # Callbacks
+
+  @impl true
+  def init(input) do
+    {:ok, MapCore.new(input)}
+  end
+
+  @impl true
+  def handle_call(:state, _from, counter) do
+    {:reply, MapCore.message(counter), counter}
+  end
+
+  @impl true
+  def handle_cast(:inc, counter) do
+    {:noreply, MapCore.inc(counter)}
+  end
+
+  def handle_cast(:dec, counter) do
+    {:noreply, MapCore.dec(counter)}
+  end
+end
