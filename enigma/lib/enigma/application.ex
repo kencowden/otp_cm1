c215ffa1c8e827b206a934283e45891bcf50e9a1
Michael Conrad <mconrad@aspirehealthcare.com>
2021-11-03 09:24:41 -0500

    add new workers for our supervisor

--- enigma/lib/enigma/application.ex
+++ enigma/lib/enigma/application.ex
@@ -11,12 +11,15 @@ defmodule Enigma.Application do
 
     children = [
       Supervisor.child_spec({Enigma.Server, :superman}, id: 1),
-      Supervisor.child_spec({Enigma.Server, :slickrick}, id: 2)
+      Supervisor.child_spec({Enigma.Server, :slickrick}, id: 2),
+      Supervisor.child_spec({Enigma.Server, :wonderwoman}, id: 3),
+      Supervisor.child_spec({Enigma.Server, :spiderman}, id: 4),
+      Supervisor.child_spec({Enigma.Server, :deadpool}, id: 5)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Enigma.Supervisor]
+    opts = [strategy: :rest_for_one, name: Enigma.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
